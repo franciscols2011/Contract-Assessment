b305d7dfd72807f4b4c9441d0c45383641fa2cbe
Francisco <franciscols2011@gmail.com>
2024-10-14 16:49:33 -0300

    Se agrego la verificación de autenticación del usuario utilizando TanStackQuery

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Header } from "@/components/header";
+import { ReactQueryProviders } from "@/providers/tanstack/react-query-providers";
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
@@ -29,8 +30,10 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<Header />
-				{children}
+				<ReactQueryProviders>
+					<Header />
+					{children}
+				</ReactQueryProviders>
 			</body>
 		</html>
 	);
--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,12 +1,28 @@
+"use client";
+
+import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
 
 const navItems: { name: string; href: string }[] = [
-	{ name: "Dashboard", href: "/" },
-	{ name: "Dashboard", href: "/" },
-	{ name: "Dashboard", href: "/" },
+	{ name: "Dashboard", href: "/dashboard" },
+	{ name: "Pricing", href: "/pricing" },
+	{ name: "Privacy Policy", href: "/privacy" },
 ];
 
+function googleSignIn(): Promise<void> {
+	return new Promise((resolve) => {
+		window.location.href = "http://localhost:8080/auth/google";
+		resolve();
+	});
+}
+
 export function Header() {
+	const pathname = usePathname();
+
+	const user = true;
+
 	return (
 		<header className="sticky px-4 top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
 			<div className="container flex h-16 items-center">
@@ -14,6 +30,28 @@ export function Header() {
 					<Link href={"/"} className="mr-6 flex items-center space-x-2">
 						LOGO
 					</Link>
+					<nav className="flex items-center space-x-7 text-sm font-medium">
+						{navItems.map((item) => (
+							<Link
+								key={item.name}
+								href={item.href}
+								className={cn(
+									"transition-colors  hover:text-foreground/80",
+									pathname === item.href
+										? "text-foreground"
+										: "text-foreground/60"
+								)}
+							>
+								{item.name}
+							</Link>
+						))}
+					</nav>
+				</div>
+
+				<div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+					<div className="w-full flex-1 md:w-auto md:flex-none space-x-2 hidden md:flex">
+						<Button onClick={googleSignIn}>Get Started</Button>
+					</div>
 				</div>
 			</div>
 		</header>
